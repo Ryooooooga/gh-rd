66e67bcd17c6e7391952fe615720731bee7196b7
Ryooooooga <eial5q265e5@gmail.com>
2023-09-23 23:46:36 +0900

    feat: donwload archive file into temp dir

--- main.ts
+++ main.ts
@@ -1,52 +1,49 @@
 #!/usr/bin/env -S deno run --no-check --no-lock --allow-read --allow-write --allow-net --allow-env --allow-run
+import { basename } from "std/path/basename.ts";
 import { loadConfig } from "./src/mod.ts";
 import {
   downloadAsset,
   extractArchive,
   findAssetURL,
 } from "./src/assets/mod.ts";
+import { ToolConfig } from "./src/config.ts";
 import {
   fetchLatestReleaseTag,
   fetchReleasedArtifactURLs,
 } from "./src/github/releases.ts";
+import { getPackageDir } from "./src/path.ts";
 
 const config = await loadConfig();
 
-async function download(name: string) {
-  console.log(`Installing ${name}...`);
-  const [user, repo] = name.split("/");
+async function download(tempDir: string, tool: ToolConfig) {
+  console.log(`Installing ${tool.name}...`);
 
-  const tag = await fetchLatestReleaseTag(user, repo);
+  const segments = tool.name.split("/");
+  if (segments.length !== 2) {
+    throw new Error("Invalid tool name");
+  }
+  const [user, repo] = segments;
+
+  const tag = tool.tag ?? await fetchLatestReleaseTag(user, repo);
   const assetsURLs = await fetchReleasedArtifactURLs(user, repo, tag);
   const assetURL = findAssetURL(assetsURLs, Deno.build.os, Deno.build.arch);
 
   if (assetURL !== undefined) {
-    const filename = `dist/${assetURL.split("/").pop()!}`;
-    console.log(`  Downloading ${assetURL} to ${filename}...`);
+    const filename = `${tempDir}/${basename(assetURL)}`;
+    console.log(`  Downloading ${assetURL}...`);
     await downloadAsset(assetURL, filename);
-    console.log(`  Extracting ${filename}...`);
-    await extractArchive(filename, `dist/${user}---${repo}`);
+    console.log(`  Extracting ${basename(filename)}...`);
+    await extractArchive(filename, getPackageDir(user, repo));
   }
 }
-const tools = [
-  // "direnv/direnv",
-  // "Ryooooooga/zabrze",
-  // "dandavison/delta",
-  // "itchyny/mmv",
-  // "BurntSushi/ripgrep",
-  // "x-motemen/ghq",
-  // "jesseduffield/lazygit",
-  // "Ryooooooga/zouch",
-  // "Ryooooooga/monkeywrench",
-  // "cli/cli",
-  // "eza-community/eza",
-  // "mikefarah/yq",
-  // "rhysd/hgrep",
-  // "denisidoro/navi",
-  // "Ryooooooga/qwy",
-  // "dbrgn/tealdeer",
-  // "himanoa/mdmg",
-  "Ryooooooga/croque",
-];
 
-await Promise.all(tools.map((name) => download(name)));
+const tempDir = await Deno.makeTempDir({ prefix: "gh-rd-" });
+try {
+  await Promise.all(config.tools.map((tool) => download(tempDir, tool)));
+} finally {
+  try {
+    await Deno.remove(tempDir, { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+}

70158df26bcd5cc696c692a6f804bcf6adda602e
Ryooooooga <eial5q265e5@gmail.com>
2024-01-26 13:51:07 +0900

    chore: update dependencies

--- src/deps/progress.ts
+++ src/deps/progress.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/x/progress@v1.4.0/mod.ts";
+export * from "https://deno.land/x/progress@v1.4.4/mod.ts";
--- src/deps/std/testing.ts
+++ src/deps/std/testing.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.207.0/assert/mod.ts";
-export * from "https://deno.land/std@0.207.0/testing/mock.ts";
+export * from "https://deno.land/std@0.213.0/assert/mod.ts";
+export * from "https://deno.land/std@0.213.0/testing/mock.ts";

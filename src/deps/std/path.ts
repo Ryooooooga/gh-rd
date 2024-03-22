55612fb8e66b54698efd80b94c7a4242e9dc5f39
Ryooooooga <eial5q265e5@gmail.com>
2024-03-22 21:42:32 +0900

    feat: update dependencies

--- src/deps/std/path.ts
+++ src/deps/std/path.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.215.0/path/mod.ts";
+export * from "https://deno.land/std@0.220.1/path/mod.ts";
--- src/deps/std/testing.ts
+++ src/deps/std/testing.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/std@0.215.0/assert/mod.ts";
-export * from "https://deno.land/std@0.215.0/testing/mock.ts";
+export * from "https://deno.land/std@0.220.1/assert/mod.ts";
+export * from "https://deno.land/std@0.220.1/testing/mock.ts";
